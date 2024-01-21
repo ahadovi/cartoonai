b04729a4b9c77423558b596700c31fd77675a89b
ahadovi <wstorage@gmail.com>
2024-01-21 16:20:21 +0600

    add rajib modification file

---
+++ client/src/constant/optionDatas.js
@@ -0,0 +1,149 @@
+export const interfaceTypeData = [
+  {
+    id: 1,
+    value: "txt2img",
+    name: "txt2img",
+  },
+  {
+    id: 2,
+    value: "img2img",
+    name: "img2img",
+  },
+];
+
+export const modelNameData = [
+  {
+    id: 1,
+    value: "Deliberate v2",
+    name: "Deliberate v2",
+  },
+  {
+    id: 2,
+    value: "dreamshaper_332BakedVaeClipFix",
+    name: "dreamshaper_332BakedVaeClipFix",
+  },
+  {
+    id: 3,
+    value: "disneyPixarCartoon_v10",
+    name: "disneyPixarCartoon_v10",
+  },
+  {
+    id: 4,
+    value: "Realistic_Vision_V5",
+    name: "Realistic_Vision_V5",
+  },
+  {
+    id: 5,
+    value: "realcartoonPixar_v2",
+    name: "realcartoonPixar_v2",
+  },
+  {
+    id: 6,
+    value: "dreamshaper_8",
+    name: "dreamshaper_8",
+  },
+  {
+    id: 7,
+    value: "realcartoon3d_v12",
+    name: "realcartoon3d_v12",
+  },
+];
+
+export const vaeNameData = [
+  {
+    id: 1,
+    value: "color101VAE_v1.safetensors",
+    name: "color101VAE_v1.safetensors",
+  },
+  {
+    id: 2,
+    value: "color101VAE_v1.safetensors",
+    name: "color101VAE_v1.safetensors",
+  },
+  {
+    id: 3,
+    value: "color101VAE_v1.safetensors",
+    name: "color101VAE_v1.safetensors",
+  },
+  {
+    id: 4,
+    value: "vae-ft-mse-840000-ema-pruned_fp16.safetensors",
+    name: "vae-ft-mse-840000-ema-pruned_fp16.safetensors",
+  },
+  {
+    id: 5,
+    value: "kl-f8-anime2_fp16.safetensors",
+    name: "kl-f8-anime2_fp16.safetensors",
+  },
+  {
+    id: 6,
+    value: "none",
+    name: "none",
+  },
+];
+
+export const moduleData = [
+  {
+    id: 1,
+    value: "Tile_Resample",
+    name: "Tile_Resample",
+  },
+  {
+    id: 2,
+    value: "lineart_realistic",
+    name: "lineart_realistic",
+  },
+  {
+    id: 3,
+    value: "scribble_xdog",
+    name: "scribble_xdog",
+  },
+  {
+    id: 4,
+    value: "lineart_standard",
+    name: "lineart_standard",
+  },
+  {
+    id: 5,
+    value: "lineart_coarse",
+    name: "lineart_coarse",
+  },
+  {
+    id: 6,
+    value: "Canny",
+    name: "Canny",
+  },
+];
+
+export const modelData = [
+  {
+    id: 1,
+    value: "control_v11f1e_sd15_tile",
+    name: "control_v11f1e_sd15_tile",
+  },
+  {
+    id: 2,
+    value: "control_v11p_sd15_lineart",
+    name: "control_v11p_sd15_lineart",
+  },
+  {
+    id: 3,
+    value: "control_v11p_sd15_scribble",
+    name: "control_v11p_sd15_scribble",
+  },
+  {
+    id: 4,
+    value: "control_v11p_sd15_lineart",
+    name: "control_v11p_sd15_lineart",
+  },
+  {
+    id: 5,
+    value: "control_v11p_sd15_lineart",
+    name: "control_v11p_sd15_lineart",
+  },
+  {
+    id: 6,
+    value: "control_v11p_sd15_canny",
+    name: "control_v11p_sd15_canny",
+  },
+];
